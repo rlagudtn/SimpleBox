89d5a7edc9ec70d6fe2fca95c7917e1d479778ec
rlagudtn <rlagudtn4510@naver.com>
2021-12-30 16:46:27 +0900

    init

---
+++ src/main/java/Duo/SimpleBox/SimpleBoxApplication.java
@@ -0,0 +1,13 @@
+package Duo.SimpleBox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleBoxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Duo/SimpleBox/SimpleBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package Duo.SimpleBox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
