9040c1a3236e26131caa8616a2b6e79acdaaeac6
rlagudtn <rlagudtn4510@naver.com>
2022-02-12 11:48:53 +0900

    before merge with multi

--- src/main/frontend/src/View/PandoraMain.js
+++ src/main/frontend/src/View/PandoraMain.js
@@ -6,13 +6,17 @@ import {Row,Col,Container} from 'react-bootstrap';
 import NavbarSub from '../LayoutComponent/NavbarSub';
 import boxicon from '../boxicon.png'
 import { Pandora } from '../DTO/Box';
-
+import './PandoraMain.css'
 function PandoraMain(){
   //검색어
   let [keyword,setKeyword]=useState("");
 
   //검색해서 얻은 결과
-  let [boxes, setBoxes]=useState([{"id":"1","name":"새박스","count":"1"},{"id":"2","name":"스프링 실전","count":"0"}]);
+  let [boxes, setBoxes]=useState([{"id":"1","name":"새박스","count":"1"},{"id":"2","name":"스프링 실전","count":"1"},
+  {"id":"1","name":"새박스","count":"1"},{"id":"2","name":"스프링 실전","count":"1"},
+  {"id":"1","name":"새박스","count":"1"},{"id":"2","name":"스프링 실전","count":"1"},
+  {"id":"1","name":"새박스","count":"1"},{"id":"2","name":"스프링 실전","count":"1"},
+  {"id":"1","name":"새박스","count":"1"},{"id":"2","name":"스프링 실전","count":"1"}]);
 
   let[pandoraList,setPandoraList]=useState([]);
 
@@ -36,18 +40,27 @@ function PandoraMain(){
   return(
     <div>
       {/* 상단 옵션 바 */}
-      <Container className='main_optionbar'> 
+      <div className="search-bar">
         <NavbarSub keyword={keyword} setKeyword={setKeyword}  setBoxes={setBoxes} />
-      </Container>
+      </div>
+      <div className="pandora-list">
+        <div className="box-container">
+          {pandoraList.map((item, index) => {
+            if (item.count > 0)
+              return <BoxItem key={index} item={item} setKeyModalToggle={setKeyModalToggle} setSelectedBox={setSelectedBox}></BoxItem>
+          })}
+        </div>
+      </div>
+      
 
       {/* 박스들 배치 */}
       <Container className='main_boxes'>
         <Row>
           {
-            pandoraList.map((item, index) => {
-              if(item.count>0)
-                return <BoxItem key={index} item={item} setKeyModalToggle={setKeyModalToggle} setSelectedBox={setSelectedBox}></BoxItem>
-            })
+            // pandoraList.map((item, index) => {
+            //   if(item.count>0)
+            //     return <BoxItem key={index} item={item} setKeyModalToggle={setKeyModalToggle} setSelectedBox={setSelectedBox}></BoxItem>
+            // })
           }
 
           {/* 박스를 클릭할 시 박스키 입력 팝업 */}
@@ -67,15 +80,19 @@ function PandoraMain(){
 // 박스 제목에 따른 각 박스
 function BoxItem(props){
   return(
-    <>
-      <Col xs={3} style={{'textAlign' : 'center'}} onClick={(e)=>{
-          props.setKeyModalToggle(true);
-          props.setSelectedBox(props.item);
-        }}>
-        <img src={boxicon}/><br/>
+    <div className='box-item' onClick={(e)=>{
+      props.setKeyModalToggle(true);
+      props.setSelectedBox(props.item);
+    }}>
+      <img src={boxicon}/><br/>
+      <div className="box-title">
         {props.item.title}
-      </Col>
-    </>
+
+      </div>
+      {/* <Col xs={3} style={{'textAlign' : 'center'}} >
+        
+      </Col> */}
+    </div>
   )
 }
 
