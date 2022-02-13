4609f906c4d69aa3ac43cb92496cd5917ef1cdf4
AnTaeWook <antk7894@naver.com>
2022-02-13 16:02:55 +0900

    make Box input

--- src/main/frontend/src/View/NewPandora.js
+++ src/main/frontend/src/View/NewPandora.js
@@ -6,12 +6,14 @@ import axios from "axios";
 import { useHistory } from "react-router-dom";
 import { useEffect, useState } from 'react';
 import _ from 'lodash';
+import './NewPandora.css'
+import upload from './upload.png';
 
 function NewPandora(){
   let history = useHistory();
   // 박스에 들어갈 파일들을 보관할 스테이트
   const [filesIn, setFilesIn] = useState([]);
-  // 삭제 버튼의 활성화 여부
+  // 드래그앤드롭 알림의 활성화 여부
   const [isFileIn, setIsFileIn] = useState(false);
   // 박스 이름
   const [boxName, setBoxName] = useState("");
@@ -52,7 +54,7 @@ function NewPandora(){
 
   function deleteFile(){
     // 파일 삭제
-    let target = document.querySelectorAll(".file_checklist input:checked");
+    let target = document.querySelectorAll(".file-list input:checked");
 
     let temp = _.cloneDeep(filesIn);
     for(var i=0; i<target.length; i++){
@@ -60,10 +62,49 @@ function NewPandora(){
     }
     setFilesIn(temp);
 
-    target = document.querySelectorAll(".file_checklist input");
+    target = document.querySelectorAll(".file-list input");
     for(i=0; i<target.length; i++){
       target[i].checked = false;
     }
+  }
+
+  // 파일 용량 확인을 위한 함수
+  function checkFileSize(bytes, decimal = 2){
+    if (bytes == 0){
+      return '0B';
+    }
+    const k = 1000;
+    const dm = decimal;
+    const sizes = ['B', 'KB', 'MB', 'GB', 'TB'];
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+    return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))}${sizes[i]}`
+  }
+
+  // 전체 체크박스를 통제하는 체크박스 함수
+  function allCheck(e){
+    let target = document.querySelectorAll(".file-list input");
+    if(e.target.checked){
+      for(let i=0; i<target.length; i++){
+        target[i].checked = true;
+      }
+    }
+    else{
+      for(let i=0; i<target.length; i++){
+        target[i].checked = false;
+      }
+    }
+  }
+
+  // 파일을 드래그하여 업로드하는 함수
+  function dropFile(e){
+    // 기존 효과 제거
+    e.preventDefault();
+
+    let temp = _.cloneDeep(filesIn);
+    for(let i=0; i<e.dataTransfer.files.length; i++){
+      temp.push(e.dataTransfer.files[i]);
+    }
+    setFilesIn(temp);
   }
 
   return (
@@ -91,26 +132,42 @@ function NewPandora(){
             <input type="number" id="quantity" name="quantity" min="1" max="20" defaultValue={5} onChange={(e)=>{
               setOpenCount(e.target.value);
             }}/>
-            <Button variant="success" style={{ 'marginLeft': '5%' }} onClick={createBox}>저장</Button>
+            <Button variant="success" style={{ 'marginLeft': '28%' }} onClick={createBox}>저장</Button>
             
             <br /><br />
-            <div className="input-group mb-3">
-              <input type="file" className="form-control" id="boxfile" accept='파일 확장자|image/*' onChange={newFile}/>
-              <label className="input-group-text" htmlFor="inputGroupFile02">Upload</label>
-            </div>
+            <label className="input-file-button" htmlFor="boxfile" >내 PC</label>
+            <input type="file" id="boxfile" style={{display:"none"}} accept='파일 확장자|image/*' onChange={newFile}/>
+            <div className='input-file-button' onClick={deleteFile}>삭제</div>
+            <br/>
 
-            <Container className="file_checklist">
-            {
-              filesIn.map((item, index) => {
-                return <FileItem index={index} fileName={item.name} key={index}></FileItem>
-              })
-            }
-            {
-              isFileIn ?
-              <input type="button" value="삭제" style={{'marginTop' : '1%'}} onClick={deleteFile}/> :
-              null
-            }
-            </Container>
+            <div className='file-list' onDrop={dropFile} onDragOver={(e) => {
+              e.preventDefault();
+            }}>
+              <div className='file-list-header'>
+                <div className='item'>
+                  <input type="checkbox" onChange={allCheck}/>
+                  <span style={{'marginLeft' : '1%'}}>파일 명</span>
+                </div>
+                
+                <div className='size'>
+                  <span>파일 용량</span>
+                </div>
+              </div>
+              <div className='file-list-files'>
+              {
+                isFileIn?
+                null:
+                <div className='drag-drop-notion'>
+                  <img src={upload}/> <b>파일을 드래그하여 업로드</b>
+                </div>
+              }
+              {
+                filesIn.map((item, index) => {
+                  return <FileItem index={index} fileName={item.name} fileSize={item.size} key={index}  checkFileSize={checkFileSize}></FileItem>
+                })
+              }
+              </div>
+            </div>
 
             {keyModalToggle!=false?
               <MakeHashCodeModal  setKeyModalToggle={setKeyModalToggle} boxName={boxName} openCount={openCount} filesIn={filesIn}/>:
@@ -127,12 +184,18 @@ function NewPandora(){
 function FileItem(props){
   return(
     <>
-      <Col>
-        <input className="form-check-input" type="checkbox" id={"file" + props.index} value={props.index}/>
-        <label className="form-check-label" htmlFor={"file" + props.index} style={{'marginLeft' : '1%'}}>
-          {props.fileName}
-        </label>
-      </Col>
+      <div className='file-item'>
+        <div className='item'>
+          <input type="checkbox" id={"file" + props.index} value={props.index}/>
+          <label className="form-check-label" htmlFor={"file" + props.index} style={{'marginLeft' : '1%'}}>
+            {props.fileName}
+          </label>
+        </div>
+        
+        <div className='size'>
+          {props.checkFileSize(props.fileSize)}
+        </div>
+      </div>
     </>
   )
 }
